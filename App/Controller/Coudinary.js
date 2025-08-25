69436c52a56ff1f2400d8b8f751dbc72f6fe2bc8
singh-jii-07 <nileshkumarsingh060@gmail.com>
2025-08-25 16:09:07 -0500

    cloudinary dowland controllers and routes done

--- App/Controller/Coudinary.js
+++ App/Controller/Coudinary.js
@@ -1,6 +1,6 @@
 import File from '../Module/Coudinary.js'
 
-export const uploadFile = async(req, res)=>{
+ const uploadFile = async(req, res)=>{
   try {
     const {fileName,fileUrl}=req.body
     if(!fileName||!fileUrl){
@@ -23,3 +23,31 @@ fileUrl
     res.status(500).json({ error: error.message });
 }
 };
+const downloadFile = async (req, res) => {
+  try {
+    const file = await File.findById(req.params.id);
+    if (!file) return res.status(404).json({ message: "File not found" });
+
+    if (Date.now() > file.expiryTime.getTime()) {
+      return res.status(410).json({ message: "Download link expired" });
+    }
+
+    file.downloadCount++;
+    await file.save();
+
+    if (req.query.redirect === "1") {
+      return res.redirect(file.fileURL);
+    }
+
+    res.json({
+      url: file.fileURL,
+      filename: file.filename,
+      downloads: file.downloadCount,
+    });
+  } catch (err) {
+    console.error("Error in downloading the files", err.message);
+    return res.status(500).json({message: "Interval server error"});
+}
+};
+
+export {uploadFile,downloadFile}
--- App/Routes/Cloudinary.js
+++ App/Routes/Cloudinary.js
@@ -1,9 +1,10 @@
-import { uploadFile } from "../Controller/Coudinary.js";
+import { uploadFile,downloadFile } from "../Controller/Coudinary.js";
 import upload from "../Controller/Multer.js";
 import express from 'express';
 
 const fileRouter = express.Router()
 
 fileRouter.post('/', upload.single('image'), uploadFile);
+fileRouter.get("/download/:id", downloadFile);
 
 export default fileRouter
