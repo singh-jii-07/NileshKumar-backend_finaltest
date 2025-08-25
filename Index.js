072529655f9fbf709fd0af1419718d58ced7099d
singh-jii-07 <nileshkumarsingh060@gmail.com>
2025-08-25 14:32:18 -0500

    db connect

--- Index.js
+++ Index.js
@@ -1,1 +1,22 @@
-console.log("hello")
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app=express()
+
+mongoose.connect(process.env.Mongoose_URL)
+.then(()=>{
+    console.log("db connect")
+    app.listen(process.env.PORT,()=>{
+    console.log(`server is running in the port:${process.env.PORT}`)
+    })
+   app.get('/',(req,res)=>{
+    res.send("hello")
+   })
+})
+
+.catch((err)=>{
+    console.log("error",err)
+})
